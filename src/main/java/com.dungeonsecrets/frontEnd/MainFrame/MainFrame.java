28e3c2175a36120f1050d33f39a3756b78ec03ed
Emil Nikolov <e.nikolov92@gmail.com>
2021-08-23 18:04:17 +0300

    Convert to Maven

---
+++ src/main/java/com.dungeonsecrets/frontEnd/MainFrame/MainFrame.java
@@ -0,0 +1,176 @@
+package com.dungeonsecrets.frontEnd.MainFrame;
+
+import com.dungeonsecrets.frontEnd.CharactersMenu.CharactersMenuPanel;
+import com.dungeonsecrets.frontEnd.CharactersMenu.ChooseCharacterPanel;
+import com.dungeonsecrets.frontEnd.CharactersMenu.CreateCharacter;
+import com.dungeonsecrets.frontEnd.MainMenu.MainMenuPanel;
+import com.dungeonsecrets.frontEnd.MultiPlayerMenu.MultiPlayerMenuPanel;
+import com.dungeonsecrets.frontEnd.OptionsMenu.OptionsMenuPanel;
+import com.dungeonsecrets.frontEnd.SinglePlayerMenu.SinglePlayerMenuPanel;
+import com.dungeonsecrets.frontEnd.GameLayout;
+import com.dungeonsecrets.frontEnd.LoginMenu;
+import com.dungeonsecrets.frontEnd.RegisterMenu;
+
+import javax.swing.*;
+
+public class MainFrame {
+
+    private static JFrame mainFrame                         = new JFrame();
+    private static MainMenuPanel mainMenuPanel              = new MainMenuPanel();
+    private static SinglePlayerMenuPanel singlePlayerMenu   = new SinglePlayerMenuPanel();
+    private static MultiPlayerMenuPanel multiPlayerMenu     = new MultiPlayerMenuPanel();
+    private static CharactersMenuPanel charactersMenu       = new CharactersMenuPanel();
+    private static OptionsMenuPanel optionsMenu             = new OptionsMenuPanel();
+    private static LoginMenu loginMenu                      = new LoginMenu();
+    private static RegisterMenu registerMenu                = new RegisterMenu();
+    private static GameLayout mainLayout                    = new GameLayout();
+    private static CreateCharacter createCharactersMenu     = new CreateCharacter();
+    private static ChooseCharacterPanel chooseCharacterMenu = new ChooseCharacterPanel();
+
+    public MainFrame(){
+
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainFrame.setUndecorated(true);
+        mainFrame.setLayout(null);
+
+        mainFrame.add(chooseCharacterMenu);
+        mainFrame.add(createCharactersMenu);
+        mainFrame.add(mainLayout);
+        mainFrame.add(registerMenu);
+        mainFrame.add(loginMenu);
+        mainFrame.add(mainMenuPanel);
+        mainFrame.add(singlePlayerMenu);
+        mainFrame.add(multiPlayerMenu);
+        mainFrame.add(charactersMenu);
+        mainFrame.add(optionsMenu);
+
+        chooseCharacterMenu.setVisible(false);
+        createCharactersMenu.setVisible(false);
+        loginMenu.setVisible(true);
+        mainLayout.setVisible(false);
+        registerMenu.setVisible(false);
+        singlePlayerMenu.setVisible(false);
+        multiPlayerMenu.setVisible(false);
+        charactersMenu.setVisible(false);
+        optionsMenu.setVisible(false);
+        mainMenuPanel.setVisible(false);
+
+        mainFrame.setVisible(true);
+
+    }
+
+    public static void openChooseCharacterMenu(){
+
+        chooseCharacterMenu.setVisible(true);
+
+    }
+
+    public static void openCreateCharactersMenu(){
+
+        createCharactersMenu.setVisible(true);
+
+    }
+
+    public static void openRegisterMenu(){
+
+        registerMenu.setVisible(true);
+
+    }
+
+    public static void openMainLayout(){
+
+        mainLayout.setVisible(true);
+
+    }
+
+    public static void openLoginMenu(){
+
+        loginMenu.setVisible(true);
+
+    }
+
+    public static void openMainMenu(){
+
+        mainMenuPanel.setVisible(true);
+
+    }
+
+    public static void openSinglePlayerMenu(){
+
+        singlePlayerMenu.setVisible(true);
+
+    }
+
+    public static void openMultiPlayerMenu(){
+
+        multiPlayerMenu.setVisible(true);
+
+    }
+
+    public static void openCharactersMenu(){
+
+        charactersMenu.setVisible(true);
+
+    }
+
+    public static void openOptionsMenu(){
+
+        optionsMenu.setVisible(true);
+
+    }
+
+    public static void closeChooseCharacterMenu(){
+
+        chooseCharacterMenu.setVisible(false);
+
+    }
+
+    public static void closeCreateCharactersMenu(){
+
+        createCharactersMenu.setVisible(false);
+
+    }
+
+    public static void closeRegisterMenu(){
+
+        registerMenu.setVisible(false);
+
+    }
+
+    public static void closeLoginMenu(){
+
+        loginMenu.setVisible(false);
+
+    }
+
+    public static void closeMainMenu(){
+
+        mainMenuPanel.setVisible(false);
+
+    }
+
+    public static void closeSinglePlayerMenu(){
+
+        singlePlayerMenu.setVisible(false);
+
+    }
+
+    public static void closeMultiPlayerMenu(){
+
+        multiPlayerMenu.setVisible(false);
+
+    }
+
+    public static void closeCharactersMenu(){
+
+        charactersMenu.setVisible(false);
+
+    }
+
+    public static void closeOptionsMenu(){
+
+        optionsMenu.setVisible(false);
+
+    }
+}
