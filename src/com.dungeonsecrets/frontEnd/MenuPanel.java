e021e63bc67ac825fcd854b37a399de56c256c1c
XP <haxp1617@gmail.com>
2021-08-23 03:26:51 +0300

    *SidePanel  -Added 3 buttons(Only "Quit" works)  -----------------------------------  Have trouble with opening panels in front

--- src/com.dungeonsecrets/frontEnd/MenuPanel.java
+++ src/com.dungeonsecrets/frontEnd/MenuPanel.java
@@ -1,27 +1,57 @@
 package com.dungeonsecrets.frontEnd;
 
+import com.dungeonsecrets.backEnd.utility.ScreenResolution;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MenuPanel {
 
-    private static JPanel menuPanel(){
+    private static JLayeredPane menuPanel(){
 
-        JPanel menuPanel = new JPanel();
+        JLayeredPane menuPanel = new JLayeredPane();
         menuPanel.setBackground(Color.yellow);
-        //Temporary positioning since we have not decided on it yet
 
-        //Temporary positioning since we have not decided on it yet
-
-        //Temporary positioning since we have not decided on it yet
-        menuPanel.setBounds(1000,1000,500,30);
+        menuPanel.setBounds((int) ((ScreenResolution.getScreenWidth())*0.817), (int) ((ScreenResolution.getScreenHeight())*0.50), (int) ((ScreenResolution.getScreenWidth())*0.19), (int) ((ScreenResolution.getScreenHeight())*0.50));
         menuPanel.setOpaque(true);
         return menuPanel;
 
     }
-    public static JPanel getMenuPanel (){
+    public static JLayeredPane getMenuPanel (){
         return  menuPanel();
     }
 
+
+
+    private static JLayeredPane menuMenu(){
+
+        JLayeredPane menuPanel = new JLayeredPane();
+        menuPanel.setBackground(Color.blue);
+        menuPanel.setBounds((int) ((ScreenResolution.getScreenWidth())*0.817), (int) ((ScreenResolution.getScreenHeight())*0.50), (int) ((ScreenResolution.getScreenWidth())*0.19), (int) ((ScreenResolution.getScreenHeight())*0.50));
+        menuPanel.setOpaque(true);
+        return menuPanel;
+
+    }
+    public static JLayeredPane getMenuMenu (){
+        return  menuMenu();
+    }
+
+
+
+//    private static JButton optionsButton(){
+//
+//        JButton options = new JButton();
+//
+//        options.setText("Options");
+//        options.setHorizontalTextPosition(JLabel.CENTER);
+//        options.setVerticalTextPosition(JLabel.BOTTOM);
+//
+//        options.setBounds((int) ((ScreenResolution.getScreenWidth())*0.90), (int) ((ScreenResolution.getScreenHeight())*0.50), (int) ((ScreenResolution.getScreenWidth())*0.05), (int) ((ScreenResolution.getScreenHeight())*0.05));
+//
+//        return options;
+//    }
+//    public static JButton getOptionsButton() {
+//        return optionsButton();
+//    }
 
 }
